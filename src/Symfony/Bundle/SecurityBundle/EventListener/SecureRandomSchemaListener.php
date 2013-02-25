b6b440b84a8b81f41949fe402733079a14aa6da1
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-02-25 19:04:37 +0100

    fixes schema when using multiple entity managers

--- src/Symfony/Bundle/SecurityBundle/EventListener/SecureRandomSchemaListener.php
+++ src/Symfony/Bundle/SecurityBundle/EventListener/SecureRandomSchemaListener.php
@@ -2,20 +2,27 @@
 
 namespace Symfony\Bundle\SecurityBundle\EventListener;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
 use Symfony\Component\Security\Core\Util\SecureRandomSchema;
 
 class SecureRandomSchemaListener
 {
     private $schema;
+    private $registry;
 
-    public function __construct(SecureRandomSchema $schema)
+    public function __construct(SecureRandomSchema $schema, ManagerRegistry $registry)
     {
         $this->schema = $schema;
+        $this->registry = $registry;
     }
 
     public function postGenerateSchema(GenerateSchemaEventArgs $args)
     {
+        if ($args->getEntityManager() !== $this->registry->getManager()) {
+            return;
+        }
+
         $this->schema->addToSchema($args->getSchema());
     }
 }
