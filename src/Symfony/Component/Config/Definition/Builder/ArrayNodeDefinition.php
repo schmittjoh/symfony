f90a5b4e0525de8abaeb4776cb5bc49f59661c31
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-09-11 21:42:52 +0200

    backports two methods

--- src/Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php
+++ src/Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php
@@ -210,6 +210,66 @@ class ArrayNodeDefinition extends NodeDefinition implements ParentNodeDefinition
     }
 
     /**
+     * Adds an "enabled" boolean to enable the current section.
+     *
+     * By default, the section is disabled. If any configuration is specified then
+     * the node will be automatically enabled:
+     *
+     * enableableArrayNode: {enabled: true, ...} # The config is enabled & default values get overridden
+     * enableableArrayNode: ~ # The config is enabled & use the default values
+     * enableableArrayNode: true # The config is enabled & use the default values
+     * enableableArrayNode: {other: value, ...} # The config is enabled & default values get overridden
+     * enableableArrayNode: {enabled: false, ...} # The config is disabled
+     * enableableArrayNode: false # The config is disabled
+     *
+     * @return ArrayNodeDefinition
+     */
+    public function canBeEnabled()
+    {
+        $this
+            ->addDefaultsIfNotSet()
+            ->treatFalseLike(array('enabled' => false))
+            ->treatTrueLike(array('enabled' => true))
+            ->treatNullLike(array('enabled' => true))
+            ->beforeNormalization()
+                ->ifArray()
+                ->then(function($v) {
+                    $v['enabled'] = isset($v['enabled']) ? $v['enabled'] : true;
+
+                    return $v;
+                })
+            ->end()
+            ->children()
+                ->booleanNode('enabled')
+                ->defaultFalse()
+        ;
+
+        return $this;
+    }
+
+    /**
+     * Adds an "enabled" boolean to enable the current section.
+     *
+     * By default, the section is enabled.
+     *
+     * @return ArrayNodeDefinition
+     */
+    public function canBeDisabled()
+    {
+        $this
+            ->addDefaultsIfNotSet()
+            ->treatFalseLike(array('enabled' => false))
+            ->treatTrueLike(array('enabled' => true))
+            ->treatNullLike(array('enabled' => true))
+            ->children()
+                ->booleanNode('enabled')
+                ->defaultTrue()
+        ;
+
+        return $this;
+    }
+
+    /**
      * Disables the deep merging of the node.
      *
      * @return ArrayNodeDefinition
